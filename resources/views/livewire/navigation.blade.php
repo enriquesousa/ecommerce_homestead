a29e43f640aa0cee3e5e31c47059c8e85cb30bdc
Enrique <enrique.sousa@gmail.com>
2022-03-26 23:57:18 -0700

    17. Diseñando el header

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,5 +1,22 @@
-<header>
-    <div class="container">
-        Hola mundo
+<header class="bg-gray-600">
+    <div class="container flex items-center h-16">
+
+        <a class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-16">
+            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+
+            <span>
+                Categorías
+            </span>
+        </a>
+
+        <a href="/" class="ml-4">
+            <x-jet-application-mark class="block h-9 w-auto" />
+        </a>
+
+        @livewire('search')
+
     </div>
 </header>
+
---
+++ resources/views/livewire/search.blade.php
@@ -0,0 +1,3 @@
+<div class="flex-1">
+    <x-jet-input type="text" class="ml-4 w-full" placeholder="Estas buscando algún producto?" />
+</div>
