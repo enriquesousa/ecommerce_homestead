083f399f60d3d89083dbee1a881fff0b19a49c13
Enrique <enrique.sousa@gmail.com>
2022-03-31 09:40:36 -0700

    23. Diseñar el submenu II

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -2,6 +2,11 @@
     #navigation-menu{
         height: calc(100vh - 4rem);
     }
+
+    /* para desplegar el submenu */
+    .navigation-link:hover .navigation-submenu {
+        display: block !important;
+    }
 </style>
 
 
@@ -109,7 +114,7 @@
                 {{-- Este ul ocupa una columna --}}
                 <ul class="bg-white">
                     @foreach ($categories as $category)
-                        <li class="text-gray-500 hover:bg-orange-500 hover:text-white">
+                        <li class="navigation-link text-gray-500 hover:bg-orange-500 hover:text-white">
 
                             <a href="" class="py-2 px-4 text-sm flex items-center">
                                 <span class="flex justify-center w-9">
@@ -118,8 +123,11 @@
                                 {{ $category->name }}
                             </a>
 
-                            <div class="bg-red-500 absolute w-3/4 h-full top-0 right-0 hidden">
+                            <div class="navigation-submenu bg-gray-100 absolute w-3/4 h-full top-0 right-0 hidden">
 
+                                {{-- componente para el submenu --}}
+                                <x-navigation-subcategories :category="$category" />
+                                
                             </div>
 
                         </li>
@@ -128,27 +136,11 @@
 
                 {{-- Este div ocupa 3 columnas --}}
                 <div class="col-span-3 bg-gray-100">
-                    <div class="grid grid-cols-4 p-4">
 
-                        <div>
-                            <p class="text-lg font-bold text-center text-gray-500 mb-3">Subcategorías</p>
+                    {{-- con : antes de la variable le decimos que lo que queremos pasar es un objeto --}}
+                    {{-- si no ponemos los : entonces estamos pasando solo la variable --}}
+                    <x-navigation-subcategories :category="$categories->first()" />
 
-                            <ul>
-                                @foreach ($categories->first()->subcategories as $subcategory)
-                                    <li>
-                                        <a href="" class="text-gray-500 inline-block font-semibold py-1 px-4 hover:text-orange-500">
-                                            {{ $subcategory->name }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-
-                        <div class="col-span-3">
-                            <img class="h-64 w-full object-cover object-center" src="{{ Storage::url($categories->first()->image) }}" alt="">
-                        </div>
-
-                    </div>
                 </div>
 
             </div>
