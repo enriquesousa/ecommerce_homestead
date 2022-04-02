63d10267d3ee8d122e657a66080a29fe609fdebe
Enrique <enrique.sousa@gmail.com>
2022-04-01 19:22:11 -0700

    25. Agregar funcionalidades al menu II
    Darle color orange cuando se de click en boton categorias

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -3,12 +3,12 @@
 
     <div class="container flex items-center h-16">
 
-        <a x-on:click="show()" class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-16">
+        <a :class="{'bg-opacity-100 text-orange-500' : open}" x-on:click="show()" class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-16">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
 
-            <span>
+            <span class="text-sm">
                 Categorías
             </span>
         </a>
