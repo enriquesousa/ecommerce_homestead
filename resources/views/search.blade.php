fc5fd2c93c759e181c5c96a959fbfacbdef230a3
Enrique <enrique.sousa@gmail.com>
2022-05-03 20:09:16 -0700

    71. Mostrar resultado de búsqueda II

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,7 +1,19 @@
 <x-app-layout>
-    <ul>
-        @foreach ($products as $product)
-            <li>{{ $product->name }}</li>
-        @endforeach
-    </ul>
+    <div class="container py-8">
+        <ul>
+            @forelse ($products as $product)
+                <x-product-list :product="$product" />
+            @empty
+                <li class="bg-white rounded-lg shadow-2xl">
+                    <div class="p-4">
+                        <p class="text-lg text-gray-700 font-semibold">Ningún producto coincide con esos parámetros</p>
+                    </div>
+                </li>                
+            @endforelse
+        </ul>
+
+        <div class="mt-4">
+            {{ $products->links() }}
+        </div>
+    </div>
 </x-app-layout>
