24359964a931e022ad3dbe9027b729353e400f76
Enrique <enrique.sousa@gmail.com>
2022-05-05 12:05:12 -0700

    77. Habilitar botones III
    Botones funcionales para color y size

--- app/Http/Livewire/UpdateCartItemSize.php
+++ app/Http/Livewire/UpdateCartItemSize.php
@@ -3,11 +3,40 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
+use App\Models\Color;
+use App\Models\Size;
 
 class UpdateCartItemSize extends Component
 {
-    public $rowId;
+    public $rowId, $qty, $quantity;
     
+    public function mount(){
+        $item = Cart::get($this->rowId);
+        $this->qty = $item->quantity;
+
+        $color = Color::where('name', $item->attributes['color'])->first();
+        $size = Size::where('name', $item->attributes['size'])->first();
+ 
+        $this->quantity = qty_available($item->id, $color->id, $size->id);
+    }
+
+    public function decrement(){
+        $this->qty = $this->qty - 1;
+        Cart::update($this->rowId, array(
+            'quantity' => -1,
+            ));
+        $this->emit('render');
+    }
+
+    public function increment(){
+        $this->qty = $this->qty + 1;
+        Cart::update($this->rowId, array(
+            'quantity' => 1,
+            ));
+        $this->emit('render');
+    }
+
     public function render()
     {
         return view('livewire.update-cart-item-size');
