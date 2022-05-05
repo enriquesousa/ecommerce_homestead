546951d5a757a641b30418f2063c0d2d0e43cd47
Enrique <enrique.sousa@gmail.com>
2022-05-04 20:00:54 -0700

    75. Habilitar botones I

--- app/Http/Livewire/UpdateCartItem.php
+++ app/Http/Livewire/UpdateCartItem.php
@@ -3,11 +3,35 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class UpdateCartItem extends Component
 {
 
-    public $rowId;
+    public $rowId, $qty, $quantity;
+
+    public function mount(){
+        $item = Cart::get($this->rowId);
+        $this->qty = $item->quantity;
+
+        $this->quantity = qty_available($item->id);
+    }
+
+    public function decrement(){
+        $this->qty = $this->qty - 1;
+        Cart::update($this->rowId, array(
+            'quantity' => -1,
+            ));
+        $this->emit('render');
+    }
+
+    public function increment(){
+        $this->qty = $this->qty + 1;
+        Cart::update($this->rowId, array(
+            'quantity' => 1,
+            ));
+        $this->emit('render');
+    }
 
     public function render()
     {
