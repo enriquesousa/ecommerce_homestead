077b69c773347e59060b3b42904c205c92d0e395
Enrique <enrique.sousa@gmail.com>
2022-05-05 10:08:38 -0700

    76. Habilitar botones II

--- app/Http/Livewire/UpdateCartItemColor.php
+++ app/Http/Livewire/UpdateCartItemColor.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Color;
 use Livewire\Component;
+use Cart;
 
 class UpdateCartItemColor extends Component
 {
-    public $rowId;
-    
+    public $rowId, $qty, $quantity;
+
+    public function mount(){
+        $item = Cart::get($this->rowId);
+        $this->qty = $item->quantity;
+
+        $color = Color::where('name', $item->attributes['color'])->first();
+
+        $this->quantity = qty_available($item->id, $color->id);
+    }
+
+    public function decrement(){
+        $this->qty = $this->qty - 1;
+        Cart::update($this->rowId, array(
+            'quantity' => -1,
+            ));
+        $this->emit('render');
+    }
+
+    public function increment(){
+        $this->qty = $this->qty + 1;
+        Cart::update($this->rowId, array(
+            'quantity' => 1,
+            ));
+        $this->emit('render');
+    }
+
     public function render()
     {
         return view('livewire.update-cart-item-color');
